f186d554d1c07b789edde052384c5d9ffdffbc00
Aditya <aditya.djauhari@mncgroup.com>
2019-05-31 16:20:59 +0700

    31-05-2019
    Sales Marketing

---
+++ app/src/main/java/com/user/salestracking/Api_Service/Api_url.java
@@ -0,0 +1,9 @@
+package com.user.salestracking.Api_Service;
+
+public class Api_url {
+    private static final String URL = "http://192.168.80.2/";
+
+    public static final String URL_REGISTER = URL + "sales_tracking/user/Api.php?apicall=register";
+    public static final String URL_LOGIN= URL + "sales_tracking/user/Api.php?apicall=login";
+    public static final String GET_DATA_DONATUR= URL + "sales_tracking/getDataDonatur/dataDonatur.php";
+}
